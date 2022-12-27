6745b37a8ca01f1a1b6d466e001c0d7c29fb499e
kaizh <kaizhetan@yahoo.com>
2022-12-27 13:30:18 +0800

    Find sys_call_table

--- mallie.c
+++ mallie.c
@@ -2,9 +2,22 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 
+// Check kernel compiled options
+// https://www.walkernews.net/2008/11/21/how-to-check-what-kernel-build-options-enabled-in-the-linux-kernel/
+// cat /boot/config-`uname -r` | grep CONFIG_NETFILTER
+
+static unsigned long *sys_call_table;
+
 static int __init mallie_module_init(void)
 {
-	printk(KERN_INFO "Driver version %s\n", EI_VERSION);
+	printk(KERN_INFO "Hi!\n");
+	sys_call_table = (void *)kallsyms_lookup_name("sys_call_table");
+
+	if (sys_call_table == NULL) {
+		printk(KERN_INFO "sys_call_table not found");
+		return -1;
+	}
+	printk(KERN_INFO "sys_call_table found at %p");
 	return 0;
 }
 
